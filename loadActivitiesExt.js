4a240ffcb43d4115569975268ecabefedeb0d141
QlNaga <pupier.quentin@gmail.com>
2021-05-20 10:39:00 +0200

    Affichage des activités en fonction de la température

---
+++ loadActivitiesExt.js
@@ -0,0 +1,28 @@
+// le nom des variables des requètes différent pour ne pas entrer en conflit avec le reste du script en amont
+
+let requestJsonURLExt = "activities.json";
+let requestJsonExt = new XMLHttpRequest();
+
+  requestJsonExt.open("GET", requestJsonURLExt);
+  requestJsonExt.responseType = "json";
+  requestJsonExt.send();
+
+  requestJsonExt.onload = function () {
+    let activities = requestJsonExt.response;
+    let activitiesExt = activities["ext-activities"];
+    let html = "";
+
+    activitiesExt.forEach(element => {
+      html += `<div class="col-lg-4 col-md-4 col-sm-12 mb-4">`
+      html += ` <div class="card" style="width: 18rem;">`;
+      html += `  <img src="${element.img}" class="card-img-top" alt="...">`;
+      html += `  <div class="card-body">`;
+      html += `    <h5 class="card-title">${element.name}</h5>`;
+      html += `    <p class="card-text">${element.description}</p>`;
+      html += `  </div>`;
+      html += ` </div>`
+      html += `</div>`
+    });
+
+    document.getElementById('activitiesList').innerHTML += html;
+  }
